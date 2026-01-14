cf61dc042329cac4faa765c13e76b1764a7986b5
Martin Erren <martin3r@KB-LEVE15-09.karl-broich.local>
2026-01-14 14:53:09 +0100

    init

--- src/Http/Controllers/NotePdfController.php
+++ src/Http/Controllers/NotePdfController.php
@@ -2,12 +2,15 @@
 
 namespace Platform\Notes\Http\Controllers;
 
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Routing\Controller;
 use Platform\Notes\Models\NotesNote;
 use Barryvdh\DomPDF\Facade\Pdf;
 
 class NotePdfController extends Controller
 {
+    use AuthorizesRequests;
+
     public function __invoke(NotesNote $notesNote)
     {
         $this->authorize('view', $notesNote);
